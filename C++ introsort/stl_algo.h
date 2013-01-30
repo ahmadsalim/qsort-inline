e40ab740802d9610773dcd39139ea5f0aae3e287
Ahmad Salim Al-Sibahi <asal@itu.dk>
2013-01-30 18:37:23 +0100

    Removed non-comparitor versions of intro sort functions

--- C++ introsort/stl_algo.h
+++ C++ introsort/stl_algo.h
@@ -47,24 +47,6 @@ inline void iter_swap(ForwardIter1 a, ForwardIter2 b) {
   iter_swap(a, b, VALUETYPE(a));
 }
 
-// median (an extension, not present in the C++ standard).
-template <class T>
-inline const T& median(const T& a, const T& b, const T& c) {
-  if (a < b)
-    if (b < c)
-      return b;
-    else if (a < c)
-      return c;
-    else
-      return a;
-  else if (a < c)
-    return a;
-  else if (b < c)
-    return c;
-  else
-    return b;
-}
-
 template <class T, class Compare>
 inline const T& median(const T& a, const T& b, const T& c, Compare comp) {
   if (comp(a, b))
@@ -80,19 +62,6 @@ inline const T& median(const T& a, const T& b, const T& c, Compare comp) {
     return c;
   else
     return b;
-}
-
-template <class RandomAccessIter, class T>
-RandomAccessIter unguarded_partition(RandomAccessIter first, RandomAccessIter last, T pivot) 
-{
-  while (true) {
-    while (*first < pivot) ++first;
-    --last;
-    while (pivot < *last) --last;
-    if (!(first < last)) return first;
-    iter_swap(first, last);
-    ++first;
-  }
 }
 
 template <class RandomAccessIter, class T, class Compare>
@@ -115,17 +84,6 @@ const int stlthreshold = 16;
 
 // sort() and its auxiliary functions. 
 
-template <class RandomAccessIter, class T>
-void unguarded_linear_insert(RandomAccessIter last, T val) {
-  RandomAccessIter next = last;
-  --next;
-  while (val < *next) {
-    *last = *next;
-    last = next;
-    --next;
-  }
-  *last = val;
-}
 
 template <class RandomAccessIter, class T, class Compare>
 void unguarded_linear_insert(RandomAccessIter last, T val, Compare comp) {
@@ -139,16 +97,6 @@ void unguarded_linear_insert(RandomAccessIter last, T val, Compare comp) {
   *last = val;
 }
 
-template <class RandomAccessIter, class T>
-inline void linear_insert(RandomAccessIter first, RandomAccessIter last, T*) {
-  T val = *last;
-  if (val < *first) {
-    copybackward(first, last, last + 1);
-    *first = val;
-  }
-  else
-    unguarded_linear_insert(last, val);
-}
 
 template <class RandomAccessIter, class T, class Compare>
 inline void linear_insert(RandomAccessIter first, RandomAccessIter last, T*, Compare comp) {
@@ -164,30 +112,12 @@ inline void linear_insert(RandomAccessIter first, RandomAccessIter last, T*, Com
 //ValueType = basetype for iterator
 //DistanceType = type for representing difference between two of the same type
 
-template <class RandomAccessIter>
-void insertion_sort(RandomAccessIter first, RandomAccessIter last) {
-  if (first == last) return; 
-  for (RandomAccessIter i = first + 1; i != last; ++i)
-    linear_insert(first, i, VALUETYPE(first));
-}
-
 template <class RandomAccessIter, class Compare>
 void insertion_sort(RandomAccessIter first,
                       RandomAccessIter last, Compare comp) {
   if (first == last) return;
   for (RandomAccessIter i = first + 1; i != last; ++i)
     linear_insert(first, i, VALUETYPE(first), comp);
-}
-
-template <class RandomAccessIter, class T>
-void unguarded_insertion_sort_aux(RandomAccessIter first, RandomAccessIter last, T*) {
-  for (RandomAccessIter i = first; i != last; ++i)
-    unguarded_linear_insert(i, T(*i));
-}
-
-template <class RandomAccessIter>
-inline void unguarded_insertion_sort(RandomAccessIter first, RandomAccessIter last) {
-  unguarded_insertion_sort_aux(first, last, VALUETYPE(first));
 }
 
 template <class RandomAccessIter, class T, class Compare>
@@ -199,16 +129,6 @@ void unguarded_insertion_sort_aux(RandomAccessIter first, RandomAccessIter last,
 template <class RandomAccessIter, class Compare>
 inline void unguarded_insertion_sort(RandomAccessIter first, RandomAccessIter last, Compare comp) {
   unguarded_insertion_sort_aux(first, last, VALUETYPE(first), comp);
-}
-
-template <class RandomAccessIter>
-void final_insertion_sort(RandomAccessIter first, RandomAccessIter last) {
-  if (last - first > stlthreshold) {
-    insertion_sort(first, first + stlthreshold);
-    unguarded_insertion_sort(first + stlthreshold, last);
-  }
-  else
-    insertion_sort(first, last);
 }
 
 template <class RandomAccessIter, class Compare>
@@ -228,19 +148,6 @@ inline Size lg(Size n) {
   return k;
 }
 
-template <class RandomAccessIter, class T>
-void partial_sort(RandomAccessIter first, RandomAccessIter middle, RandomAccessIter last, T*) {
-  make_heap(first, middle);
-  for (RandomAccessIter i = middle; i < last; ++i)
-    if (*i < *first) 
-      pop_heap(first, middle, i, T(*i), DISTANCETYPE(first));
-  sort_heap(first, middle);
-}
-
-template <class RandomAccessIter>
-inline void partial_sort(RandomAccessIter first, RandomAccessIter middle, RandomAccessIter last) {
-  partial_sort(first, middle, last, VALUETYPE(first));
-}
 
 template <class RandomAccessIter, class T, class Compare>
 void partial_sort(RandomAccessIter first, RandomAccessIter middle, RandomAccessIter last, T*, Compare comp) {
@@ -254,23 +161,6 @@ void partial_sort(RandomAccessIter first, RandomAccessIter middle, RandomAccessI
 template <class RandomAccessIter, class Compare>
 inline void partial_sort(RandomAccessIter first, RandomAccessIter middle, RandomAccessIter last, Compare comp) {
   partial_sort(first, middle, last, VALUETYPE(first), comp);
-}
-
-template <class RandomAccessIter, class T, class Size>
-void introsort_loop(RandomAccessIter first, RandomAccessIter last, T*, Size depthlimit)
-{
-  while (last - first > stlthreshold) {
-    if (depthlimit == 0) {
-      partial_sort(first, last, last);
-      return;
-    }
-    --depthlimit;
-    RandomAccessIter cut =
-      unguarded_partition(first, last,
-                            T(median(*first, *(first + (last - first)/2), *(last - 1))));
-    introsort_loop(cut, last, (T*) 0, depthlimit);
-    last = cut;
-  }
 }
 
 template <class RandomAccessIter, class T, class Size, class Compare>
@@ -290,14 +180,6 @@ void introsort_loop(RandomAccessIter first, RandomAccessIter last, T*, Size dept
        comp);
     introsort_loop(cut, last, (T*) 0, depthlimit, comp);
     last = cut;
-  }
-}
-
-template <class RandomAccessIter>
-inline void sort(RandomAccessIter first, RandomAccessIter last) {
-  if (first != last) {
-    introsort_loop(first, last, VALUETYPE(first), lg(last - first) * 2);
-    final_insertion_sort(first, last);
   }
 }
 
--- C++ introsort/stl_heap.h
+++ C++ introsort/stl_heap.h
@@ -33,31 +33,6 @@
 namespace std {
 // Heap-manipulation functions: push_heap, pop_heap, make_heap, sort_heap.
 
-template <class RandomAccessIterator, class Distance, class T>
-void push_heap(RandomAccessIterator first, Distance holeIndex, Distance topIndex, T value)
-{
-  Distance parent = (holeIndex - 1) / 2;
-  while (holeIndex > topIndex && *(first + parent) < value) {
-    *(first + holeIndex) = *(first + parent);
-    holeIndex = parent;
-    parent = (holeIndex - 1) / 2;
-  }
-  *(first + holeIndex) = value;
-}
-
-template <class RandomAccessIterator, class Distance, class T>
-inline void push_heap_aux(RandomAccessIterator first, RandomAccessIterator last, Distance*, T*)
-{
-  push_heap(first, Distance((last - first) - 1), Distance(0), T(*(last - 1)));
-}
-
-template <class RandomAccessIterator>
-inline void 
-push_heap(RandomAccessIterator first, RandomAccessIterator last)
-{
-  push_heap_aux(first, last, DISTANCETYPE(first), VALUETYPE(first));
-}
-
 template <class RandomAccessIterator, class Distance, class T, class Compare>
 void
 push_heap(RandomAccessIterator first, Distance holeIndex, Distance topIndex, T value, Compare comp)
@@ -71,8 +46,7 @@ push_heap(RandomAccessIterator first, Distance holeIndex, Distance topIndex, T v
   *(first + holeIndex) = value;
 }
 
-template <class RandomAccessIterator, class Compare,
-          class Distance, class T>
+template <class RandomAccessIterator, class Compare, class Distance, class T>
 inline void 
 push_heap_aux(RandomAccessIterator first, RandomAccessIterator last, Compare comp, Distance*, T*) 
 {
@@ -84,49 +58,6 @@ inline void
 push_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
 {
   push_heap_aux(first, last, comp, DISTANCETYPE(first), VALUETYPE(first));
-}
-
-template <class RandomAccessIterator, class Distance, class T>
-void 
-adjust_heap(RandomAccessIterator first, Distance holeIndex, Distance len, T value)
-{
-  Distance topIndex = holeIndex;
-  Distance secondChild = 2 * holeIndex + 2;
-  while (secondChild < len) {
-    if (*(first + secondChild) < *(first + (secondChild - 1)))
-      secondChild--;
-    *(first + holeIndex) = *(first + secondChild);
-    holeIndex = secondChild;
-    secondChild = 2 * (secondChild + 1);
-  }
-  if (secondChild == len) {
-    *(first + holeIndex) = *(first + (secondChild - 1));
-    holeIndex = secondChild - 1;
-  }
-  push_heap(first, holeIndex, topIndex, value);
-}
-
-template <class RandomAccessIterator, class T, class Distance>
-inline void 
-pop_heap(RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator result, T value, Distance*)
-{
-  *result = *first;
-  adjust_heap(first, Distance(0), Distance(last - first), value);
-}
-
-template <class RandomAccessIterator, class T>
-inline void 
-pop_heap_aux(RandomAccessIterator first, RandomAccessIterator last, T*)
-{
-  pop_heap(first, last - 1, last - 1, 
-             T(*(last - 1)), DISTANCETYPE(first));
-}
-
-template <class RandomAccessIterator>
-inline void pop_heap(RandomAccessIterator first, 
-                     RandomAccessIterator last)
-{
-  pop_heap_aux(first, last, VALUETYPE(first));
 }
 
 template <class RandomAccessIterator, class Distance, class T, class Compare>
@@ -171,30 +102,7 @@ pop_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
   pop_heap_aux(first, last, VALUETYPE(first), comp);
 }
 
-template <class RandomAccessIterator, class T, class Distance>
-void 
-make_heap(RandomAccessIterator first, RandomAccessIterator last, T*, Distance*)
-{
-  if (last - first < 2) return;
-  Distance len = last - first;
-  Distance parent = (len - 2)/2;
-    
-  while (true) {
-    adjustheap(first, parent, len, T(*(first + parent)));
-    if (parent == 0) return;
-    parent--;
-  }
-}
-
-template <class RandomAccessIterator>
-inline void 
-make_heap(RandomAccessIterator first, RandomAccessIterator last)
-{
-  make_heap(first, last, VALUETYPE(first), DISTANCETYPE(first));
-}
-
-template <class RandomAccessIterator, class Compare,
-          class T, class Distance>
+template <class RandomAccessIterator, class Compare, class T, class Distance>
 void
 make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp, T*, Distance*)
 {
@@ -214,13 +122,6 @@ inline void
 make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
 {
   make_heap(first, last, comp, VALUETYPE(first), DISTANCETYPE(first));
-}
-
-template <class RandomAccessIterator>
-void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
-{
-  while (last - first > 1)
-    pop_heap(first, last--);
 }
 
 template <class RandomAccessIterator, class Compare>
